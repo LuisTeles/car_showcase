108b4fa059a63c82e65e9ac47b68461e9ddf6659
Luis Teles <devzluis@gmail.com>
2023-11-07 12:39:25 -0300

    cars description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+import './globals.css';
 
-import { Footer, Navbar } from '@/components' 
-
+import { Footer, Navbar } from '@/components';
 
 export const metadata: Metadata = {
   title: 'Car Hub',
-  description: 'Discover the best car in the world',
-}
+  description: 'Discover the best cars in the world',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className='relative'>
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
